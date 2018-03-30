dd1b8fc71d434f6abe3f2fde64ab430c34d401c2
sevenflash <sevenflash@yandex.ru>
2018-03-30 15:35:03 +0300

    init

---
+++ migrations/2_token.js
@@ -0,0 +1,23 @@
+const Token = artifacts.require("RealityToken");
+const SafeMath = artifacts.require("SafeMath");
+
+module.exports = function(deployer, network, accounts) {
+	const cells = 100000000000000; // one hundred trillions
+	const amount = new web3.BigNumber(web3.toWei(cells, 'ether'));
+	const name = "REALITY";
+	const symbol = "REAL";
+	const granularity = new web3.BigNumber(web3.toWei(1, 'ether'));
+	const data = "";
+
+	deployer.then(function() {
+		return deployer.deploy(SafeMath);
+	}).then(function() {
+		return deployer.link(SafeMath, Token);
+	}).then(function() {
+		return deployer.deploy(Token, name, symbol, granularity);
+	}).then(function() {
+		return Token.deployed();
+	}).then(function(instance) {
+		return instance.mint(accounts[0], amount, data);
+	});
+}
